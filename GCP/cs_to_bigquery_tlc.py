949a2040ff7a396c2f54894808080de9fa13b002
leocorbur <113495542+leocorbur@users.noreply.github.com>
2024-01-17 12:06:13 -0500

    update python scripts

--- GCP/cs_to_bigquery_tlc.py
+++ GCP/cs_to_bigquery_tlc.py
@@ -1,5 +1,11 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
 from pyspark.sql.types import StructType, StructField, StringType, TimestampType, DoubleType, LongType
+from pyspark.sql.functions import col
+from pyspark.sql import functions as F
+from datetime import datetime
+
+# Tiempo de Ejecución
+start_time = datetime.now()
 
 # Configura tu SparkSession
 spark = SparkSession.builder.appName("process_and_load_to_bq").getOrCreate()
@@ -7,8 +13,15 @@ spark = SparkSession.builder.appName("process_and_load_to_bq").getOrCreate()
 # Evita que se genere _success
 spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
 
-years = ["2020"]
+years = ["2023"]
 months = ["01", "02"]
+
+# Registro
+file_name = "fhvhv_tripdata"
+file_format = "parquet"
+initial_record_count = 0
+null_values_count = []
+duplicate_records_count = 0
 
 for year in years:
     for month in months:
@@ -18,6 +31,12 @@ for year in years:
         try:
             # Lee el archivo Parquet en un DataFrame de Spark
             df = spark.read.parquet(gcs_path_input)
+
+            # Registro
+            initial_record_count += df.count()
+            initial_column_count = len(df.columns)
+            null_values_count += list(df.select([F.sum(col(c).isNull().cast("int")).alias(c) for c in df.columns]).collect()[0].asDict().values())
+            duplicate_records_count += df.count() - df.dropDuplicates().count()
 
             # Elimina duplicados basados en todas las columnas
             df = df.dropDuplicates()
@@ -40,6 +59,10 @@ for year in years:
             # Elimina valores nulos
             df = df.na.drop()
 
+            # Registro
+            final_record_count = df.count()
+            final_column_count = len(df.columns)
+
             # Configura las opciones para BigQuery
             bigquery_project = "proyecto-de-prueba-23"
             bigquery_dataset = "greenMiles"
@@ -51,6 +74,36 @@ for year in years:
                 .option("table", f"{bigquery_project}:{bigquery_dataset}.{bigquery_table}") \
                 .mode("append") \
                 .save()
+            
+            end_time = datetime.now()
+            execution_time = end_time - start_time
+            execution_time_seconds = execution_time.total_seconds()
+            execution_date = start_time.date()
+
+            registro = [file_name, file_format, initial_record_count, final_record_count,
+            initial_column_count, final_column_count, sum(null_values_count),
+             duplicate_records_count, execution_time_seconds, execution_date ]
+            
+            # Crear DataFrame de Spark a partir de la lista registro
+            registro_df = spark.createDataFrame([Row(*registro)], ["file_name", "file_format", "initial_record_count", 
+                                                      "final_record_count", "initial_column_count", 
+                                                      "final_column_count", "null_values_sum", 
+                                                      "duplicate_records_count", "execution_time_seconds", 
+                                                      "execution_date"])
+            
+            # Cambiar los tipos de datos en el DataFrame de registro
+            registro_df = registro_df.withColumn("null_values_sum", col("null_values_sum").cast("integer"))
+
+            # Tabla de Auditoria
+            bigquery_project = "proyecto-de-prueba-23"
+            bigquery_dataset = "greenMiles"
+            bigquery_table = "Auditoria"
+
+            registro_df.write.format("bigquery") \
+            .option("temporaryGcsBucket", "tmpr_files") \
+            .option("table", f"{bigquery_project}:{bigquery_dataset}.{bigquery_table}") \
+            .mode("append") \
+            .save()
 
         except Exception as e:
             print(f"Error procesando el archivo {gcs_path_input}: {str(e)}")
--- GCP/cs_to_bq_fuelConsumption.py
+++ GCP/cs_to_bq_fuelConsumption.py
@@ -1,6 +1,11 @@
-from pyspark.sql import SparkSession, Window
+from pyspark.sql import SparkSession, Window, Row
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
 from pyspark.sql.functions import col, monotonically_increasing_id, desc, row_number
+from pyspark.sql import functions as F
+from datetime import datetime
+
+# Tiempo de Ejecución
+start_time = datetime.now()
 
 # Crear una sesión de Spark
 spark = SparkSession.builder.appName("FuelConsumption").getOrCreate()
@@ -30,43 +35,68 @@ gcs_path_input = ["gs://raw-files/csv/fuelConsumption_2020.csv",
                   "gs://raw-files/csv/fuelConsumption_2021.csv", 
                   "gs://raw-files/csv/fuelConsumption_2022.csv", 
                   "gs://raw-files/csv/fuelConsumption_2023.csv"]
+# Registro
+file_name = "fuelConsumption files"
+file_format = "csv"
+initial_record_count = 0
+null_values_count = []
+duplicate_records_count = 0
 
 # DataFrame para almacenar datos combinados
 df_combinado = spark.createDataFrame([], schema=schema)
 
+
 # Iterar sobre cada archivo
 for archivo in gcs_path_input:
-    ruta_archivo = archivo
+    try:
+
+        ruta_archivo = archivo
+        
+        # Leer datos del archivo
+        df_temp = spark.read.csv(ruta_archivo, schema=schema, header=True)
+
+        # Registro
+        initial_record_count += df_temp.count()
+        initial_column_count = len(df_temp.columns)
+        
+        # Filtrar y retirar las filas que no son necesarias
+        df_temp = df_temp.filter((df_temp["Model10"] != "Year"))
+
+        # Registro
+        null_values_count += list(df_temp.select([F.sum(col(c).isNull().cast("int")).alias(c) for c in df_temp.columns]).collect()[0].asDict().values())
+        duplicate_records_count += df_temp.count() - df_temp.dropDuplicates().count()
+        
+        # Eliminar filas con todas las entradas nulas
+        df_temp = df_temp.na.drop(how='all')
+        
+        # Agregar índices usando row_number()
+        windowSpec = Window.orderBy("Smog")
+        df_temp = df_temp.withColumn("Index", row_number().over(windowSpec))
+        
+        # Filtrar y retirar las filas que no son necesarias
+        df_temp = df_temp.filter((df_temp["Index"] > 8))
+        
+        # Seleccionar las columnas deseadas y reorganizarlas
+        columnas_seleccionadas = ["Model10","Make", "Model12", "Vehicle Class", "Engine Size", "Cylinders",
+                                "Transmission", "Fuel", "Fuel Consumption", "_c9", "_c10",
+                                "_c11", "CO2 Emissions", "CO2", "Smog" ]
+        df_temp = df_temp.select(columnas_seleccionadas)
+        
+        # Reemplazar NaN o null con 0 en todas las columnas
+        df_temp = df_temp.fillna(0)
+        
     
-    # Leer datos del archivo
-    df_temp = spark.read.csv(ruta_archivo, schema=schema, header=True)
+        
+        # Unir los DataFrames
+        df_combinado = df_combinado.union(df_temp)
     
-    # Filtrar y retirar las filas que no son necesarias
-    df_temp = df_temp.filter((df_temp["Model10"] != "Year"))
-    
-    # Eliminar filas con todas las entradas nulas
-    df_temp = df_temp.na.drop(how='all')
-    
-    # Agregar índices usando row_number()
-    windowSpec = Window.orderBy("Smog")
-    df_temp = df_temp.withColumn("Index", row_number().over(windowSpec))
-    
-    # Filtrar y retirar las filas que no son necesarias
-    df_temp = df_temp.filter((df_temp["Index"] > 8))
-    
-    # Seleccionar las columnas deseadas y reorganizarlas
-    columnas_seleccionadas = ["Model10","Make", "Model12", "Vehicle Class", "Engine Size", "Cylinders",
-                              "Transmission", "Fuel", "Fuel Consumption", "_c9", "_c10",
-                               "_c11", "CO2 Emissions", "CO2", "Smog" ]
-    df_temp = df_temp.select(columnas_seleccionadas)
-    
-    # Reemplazar NaN o null con 0 en todas las columnas
-    df_temp = df_temp.fillna(0)
-    
-   
-    
-    # Unir los DataFrames
-    df_combinado = df_combinado.union(df_temp)
+    except Exception as e:
+
+        # Registro de excepciones
+        error_log = f"Error processing file: {str(e)}"
+# Registro
+final_record_count = df_combinado.count()
+final_column_count = len(df_combinado.columns)
 
  # Cambiar los tipos de datos
 for columna in ["Engine Size", "Fuel Consumption", "_c9", "_c10"]:
@@ -101,6 +131,36 @@ df_combinado.write.format("bigquery") \
 .option("temporaryGcsBucket", "tmpr_files") \
 .option("table", f"{bigquery_project}:{bigquery_dataset}.{bigquery_table}") \
 .mode("overwrite") \
+.save()
+
+end_time = datetime.now()
+execution_time = end_time - start_time
+execution_time_seconds = execution_time.total_seconds()
+execution_date = start_time.date()
+
+registro = [file_name, file_format, initial_record_count, final_record_count,
+            initial_column_count, final_column_count, sum(null_values_count),
+             duplicate_records_count, execution_time_seconds, execution_date ]
+
+# Crear DataFrame de Spark a partir de la lista registro
+registro_df = spark.createDataFrame([Row(*registro)], ["file_name", "file_format", "initial_record_count", 
+                                                      "final_record_count", "initial_column_count", 
+                                                      "final_column_count", "null_values_sum", 
+                                                      "duplicate_records_count", "execution_time_seconds", 
+                                                      "execution_date"])
+
+# Cambiar los tipos de datos en el DataFrame de registro
+registro_df = registro_df.withColumn("null_values_sum", col("null_values_sum").cast("integer"))
+
+# Tabla de Auditoria
+bigquery_project = "proyecto-de-prueba-23"
+bigquery_dataset = "greenMiles"
+bigquery_table = "Auditoria"
+
+registro_df.write.format("bigquery") \
+.option("temporaryGcsBucket", "tmpr_files") \
+.option("table", f"{bigquery_project}:{bigquery_dataset}.{bigquery_table}") \
+.mode("append") \
 .save()
 
 
